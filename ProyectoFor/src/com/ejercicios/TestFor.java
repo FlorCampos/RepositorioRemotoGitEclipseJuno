5838bb8733791d9e565901f3559e3fe2c8548534
FlorCampos <fcfcamposflores@gmail.com>
2016-05-15 12:54:30 -0500

    Commit1

---
+++ ProyectoFor/src/com/ejercicios/TestFor.java
@@ -0,0 +1,14 @@
+package com.ejercicios;
+
+public class TestFor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pri¿obando repositorios");
+
+	}
+
+}
---
+++ ProyectoWhile/src/com/ejercicios/TestWhile.java
@@ -0,0 +1,16 @@
+package com.ejercicios;
+
+public class TestWhile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("saludos");
+		System.out.println("prueba");
+		System.out.println("guradar");
+
+	}
+
+}
